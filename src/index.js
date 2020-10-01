bf624b87cf5cd5b2774f74c9c204afb1dad69b80
Ваше имя@ <you@example.com>
2020-10-01 14:20:33 +0300

    feat: add revers int solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+       n = n.toString().substr(1); 
+    } else {
+        n = n.toString(); 
+    }
+    return Number(n.split('').reverse().join(''));  
 }
